2b9de8ffb6c07257b202462a51bb9530b9c25840
jagorczyk <mrigork2@gmail.com>
2026-06-15 20:57:32 +0200

    Fix CORS by adding www.gymlos.pl

--- src/main/java/com/jagorczyk/gymManagement/config/WebConfig.java
+++ src/main/java/com/jagorczyk/gymManagement/config/WebConfig.java
@@ -22,7 +22,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "https://gymlos.pl", "http://gymlos.pl")
+                .allowedOrigins("http://localhost:5173", "https://gymlos.pl", "http://gymlos.pl", "https://www.gymlos.pl", "http://www.gymlos.pl")
                 .allowedMethods("*")
                 .allowedHeaders("*");
     }
